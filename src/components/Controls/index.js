1bc30033fded970b4ec09ef66d5e1b5bfbcc76c9
Louis <louiswtf@users.noreply.github.com>
2017-05-08 22:08:01 +1000

    CSS tidy up

--- src/components/Controls/index.js
+++ src/components/Controls/index.js
@@ -26,8 +26,13 @@ class Controls extends Component {
         const periods = [1,2,3,4,5,6,7,'lanthanides','actinides'];
         let i = 0;
         let periodButtons = periods.map((period) => {
-            let label = (typeof period === 'string') ? period.charAt(0).toUpperCase() + period.slice(1) : period;
+            let label;
             let buttonKey = 'periodbutton' + i++;
+            if (typeof period === 'string') {
+                label = period.charAt(0).toUpperCase() + period.slice(1);
+            } else {
+                label = period;
+            }
             return <button key={buttonKey} value={period} onClick={this.handleFilterPeriod}>{label}</button>
         });
         let tempValue = 0;
@@ -36,14 +41,10 @@ class Controls extends Component {
             tempValue = this.props.filterTemperature;
             tempDisplay = tempValue + ' K';
         }
-        const elementsKey = [
+        const elementsCategories = [
             {
-                name: 'Diatomic Non-Metal',
-                category: 'diatomic-nonmetal'
-            },
-            {
-                name: 'Noble Gas',
-                category: 'noble-gas'
+                name: 'Actinide',
+                category: 'actinide'
             },
             {
                 name: 'Alkali Metal',
@@ -54,8 +55,20 @@ class Controls extends Component {
                 category: 'alkaline-earth-metal'
             },
             {
+                name: 'Diatomic Non-Metal',
+                category: 'diatomic-nonmetal'
+            },
+            {
+                name: 'Lanthanide',
+                category: 'lanthanide'
+            },
+            {
                 name: 'Metalloid',
                 category: 'metalloid'
+            },
+            {
+                name: 'Noble Gas',
+                category: 'noble-gas'
             },
             {
                 name: 'Polyatomic Non-Metal',
@@ -70,19 +83,11 @@ class Controls extends Component {
                 category: 'transition-metal'
             },
             {
-                name: 'Lanthanide',
-                category: 'lanthanide'
-            },
-            {
-                name: 'Actinide',
-                category: 'actinide'
-            },
-            {
                 name: 'Unknown',
                 category: 'unknown'
             }
         ];
-        let keyItems = elementsKey.map((item) => {
+        let categoriesItems = elementsCategories.map((item) => {
             let className = 'item ' + item.category;
             if (this.props.filterCategory === item.category) className += ' active';
             return <div key={'element-key-' + item.category} data-category={item.category} className={className} onClick={this.handleFilterCategory}>{item.name}</div>
@@ -95,9 +100,9 @@ class Controls extends Component {
                 <h3>Temperature</h3>
                 <input type="range" min="0" max="6000" value={tempValue} onChange={this.handleFilterTemperature} />
                 <span id="tempvalue">{tempDisplay}</span>
-                <h3>Key</h3>
-                <div className="key">
-                    {keyItems}
+                <h3>Categories</h3>
+                <div className="categories">
+                    {categoriesItems}
                 </div>
             </div>
         );
